0d4f7ede2b94fdb5e44ad7a1bc6b7c7af9e0bac9
Francisco Trindade <francisco.alexandre.trindade@gmail.com>
2015-05-13 05:19:24 +0100

    top bar photo / name
    top bar photo / name

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < Clearance::UsersController
+  before_action :set_user, only: :show
+  
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  private
+
+  def user_from_params
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+    nome = user_params.delete(:nome)
+
+    clearance_configuration(email, password, nome)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def clearance_configuration(email, password, nome)
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.email = email
+      user.password = password
+      user.nome = nome
+    end
+  end
+end
